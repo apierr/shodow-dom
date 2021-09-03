a1d26c514cc6473f28420dca64a8772a6e63034c
Antonio Pierro <antonio.pierro@gmail.com>
2021-09-03 19:13:04 +0200

    feat: use shadowDom with template

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 var host = document.querySelector('#shadowHost');
 var shadowRoot = host.attachShadow({ mode: 'open' });
-var div = document.createElement('div');
 
-div.textContent = 'Can you see me';
+var div = document.createElement('div');
+div.textContent = 'I am a shodow element';
 div.className = 'x';
 shadowRoot.appendChild(div);
+
+shadowRoot.appendChild(document.querySelector('#tmpl').content);
